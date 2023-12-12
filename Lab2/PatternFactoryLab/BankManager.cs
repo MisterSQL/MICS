8e37579272f9ffaee75f8bd31fb69ed7cbd0280f
Suren Poghosyan <dendy20123@gmail.com>
2023-12-12 13:44:15 -0800

    Laba 2

---
+++ Lab2/PatternFactoryLab/BankManager.cs
@@ -0,0 +1,10 @@
+﻿ namespace PatternFactoryLab
+{
+    // This interface defines the contract for bank managers.
+    interface IBankManager
+    {
+
+        // Method to process client requests.
+        void processСlientRequest();
+    }
+}
